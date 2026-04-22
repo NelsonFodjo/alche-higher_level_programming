161336153d26ae00c1c5bb8d76881142a9dca1e5
NelsonFodjo <n.fodjokamd@alustudent.com>
2026-04-22 05:51:44 -0700

    nice

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 import random
-#number = random.randint(-10000, 10000)
-number = -98
+number = random.randint(-10000, 10000)
 if number < 0:
     last = -(abs(number) % 10)
 else:
