6480d1201a1bfd3733fe3da2b31550841ffdd76f
NelsonFodjo <n.fodjokamd@alustudent.com>
2026-04-23 22:40:25 +0400

    nice

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,6 +1,15 @@
-#!/usr/bin/bash
 #!/usr/bin/python3
-from add_0 import add
+def add(a, b):
+    """My addition function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a + b
+    """
+    return (a + b)
 
 if __name__ == "__main__":
     a = 1
