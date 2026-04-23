ca8d045a5aeb9f3e5f04837e70b59931cd518371
NelsonFodjo <n.fodjokamd@alustudent.com>
2026-04-24 00:57:18 +0400

    nice

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,28 +1,26 @@
 #!/usr/bin/python3
 import sys
-from calculator_1 import add, sub, mul, div
+import calculator_1
 
 if __name__ == "__main__":
-    # Check for correct number of arguments (script name + 3 args = 4)
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
     a = int(sys.argv[1])
-    operator = sys.argv[2]
+    op = sys.argv[2]
     b = int(sys.argv[3])
 
-    # Mapping operators to their respective imported functions
-    if operator == "+":
-        result = add(a, b)
-    elif operator == "-":
-        result = sub(a, b)
-    elif operator == "7":
-        result = mul(a, b)
-    elif operator == "/":
-        result = div(a, b)
-    else:
-        print("Unknown operator. Available operators: +, -, 7 and /")
+    ops = {
+        "+": calculator_1.add,
+        "-": calculator_1.sub,
+        "*": calculator_1.mul,
+        "/": calculator_1.div
+    }
+
+    if op not in ops:
+        print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
 
-    print("{} {} {} = {}".format(a, operator, b, result))
+    result = ops[op](a, b)
+    print("{} {} {} = {}".format(a, op, b, result))
