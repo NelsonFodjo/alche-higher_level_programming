c9bf5dbc28b76e8578a5e87e8e0863c616a0ac18
NelsonFodjo <n.fodjokamd@alustudent.com>
2026-04-24 01:35:43 +0400

    nice

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,21 +1,21 @@
 #!/usr/bin/python3
+"""Calculator program"""
 import sys
-from calculator_1 import *
- 
- 
+import calculator_1 as calc
+
 if __name__ == "__main__":
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
- 
+
     a = int(sys.argv[1])
     op = sys.argv[2]
     b = int(sys.argv[3])
- 
-    ops = {"+": add, "-": sub, "*": mul, "/": div}
- 
+
+    ops = {"+": calc.add, "-": calc.sub, "*": calc.mul, "/": calc.div}
+
     if op not in ops:
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
- 
+
     print("{} {} {} = {}".format(a, op, b, ops[op](a, b)))
