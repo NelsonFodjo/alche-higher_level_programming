e6311252fe6871bfe850df60e2868b91d7579a2a
NelsonFodjo <n.fodjokamd@alustudent.com>
2026-04-25 00:32:32 +0400

    nice

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -2,3 +2,11 @@
 def simple_delete(a_dictionary, key=""):
     a_dictionary.pop(key, None)
     return a_dictionary
+
+a_dictionary = {
+    'a': 1,
+    'b': 2,
+    'c': 3,
+    'd': 4,
+    'e': 5
+}
