24666241d632927b14eb75b20762971d16c7d7c8
NelsonFodjo <n.fodjokamd@alustudent.com>
2026-04-24 18:22:31 +0400

    nice

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+def uniq_add(my_list=[]):
+    sum = 0
+    unique = []
+    for i in range(len(my_list)):
+        if my_list[i] not in unique:
+            sum += my_list[i]
+    return sum
