6f850ef8ef508aaee7307a1666104369cd4971e9
NelsonFodjo <n.fodjokamd@alustudent.com>
2026-04-25 00:46:33 +0400

    nice

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,15 +1,3 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    return max(a_dictionary.values())
-
-
-
-a_dictionary = {
-    'a': 1,
-    'b': 2,
-    'c': 3,
-    'd': 4,
-    'e': 5
-}
-
-print(best_score(a_dictionary))
+    return max(a_dictionary, key=a_dictionary.get)
