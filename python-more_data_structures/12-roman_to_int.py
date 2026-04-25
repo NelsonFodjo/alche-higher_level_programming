132f272b0f1434f6b31dbd1fafa38bba07c9f09f
NelsonFodjo <n.fodjokamd@alustudent.com>
2026-04-26 00:57:14 +0400

    nice

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -3,8 +3,12 @@ def roman_to_int(roman_string):
     roman = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     number = 0
     i = 0
+    if not roman_string.isinstance(str):
+        return None
+    
     while (i < len(roman_string)):
-        if i + 1 < len(roman_string) and roman[roman_string[i]] < roman[roman_string[i+1]]:
+        condi = roman[roman_string[i]] < roman[roman_string[i+1]]
+        if i + 1 < len(roman_string) and condi:
             number = number + roman[roman_string[i+1]] - roman[roman_string[i]]
             i = i + 2
         else:
