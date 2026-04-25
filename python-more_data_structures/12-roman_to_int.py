0f54437f5a5bc910f6f28678467ed3453508b47e
NelsonFodjo <n.fodjokamd@alustudent.com>
2026-04-26 00:52:21 +0400

    Enhanced 1 file with improvements
    Changes include: Modified 12-roman_to_int.py
    
    Added 18 new lines

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+def roman_to_int(roman_string):
+    roman = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    number = 0
+    i = 0
+    while (i < len(roman_string)):
+        if i + 1 < len(roman_string) and roman[roman_string[i]] < roman[roman_string[i+1]]:
+            number = number + roman[roman_string[i+1]] - roman[roman_string[i]]
+            i = i + 2
+        else:
+            number += roman[roman_string[i]]
+            i = i + 1
+    return number
+
+
+
+#664 = DCLXIV
+
+print(roman_to_int("DCLXIV"))
