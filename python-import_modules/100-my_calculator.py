9083e1fa5e6679929f105857530d34103146c01a
NelsonFodjo <n.fodjokamd@alustudent.com>
2026-04-26 22:41:29 +0400

    sdf

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,24 +1,11 @@
 #!/usr/bin/python3
-import sys
 from calculator_1 import add, sub, mul, div
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
+    a = 10
+    b = 5
 
-    a = int(sys.argv[1])
-    op = sys.argv[2]
-    b = int(sys.argv[3])
-
-    if op == "+":
-        print("{} {} {} = {}".format(a, op, b, add(a, b)))
-    elif op == "-":
-        print("{} {} {} = {}".format(a, op, b, sub(a, b)))
-    elif op == ".":
-        print("{} {} {} = {}".format(a, op, b, mul(a, b)))
-    elif op == "/":
-        print("{} {} {} = {}".format(a, op, b, div(a, b)))
-    else:
-        print("Unknown operator. Available operators: +, -, . and /")
-        sys.exit(1)
+print("{} + {} = {}".format(a, op, b, add(a, b)))
+print("{} - {} = {}".format(a, op, b, sub(a, b)))
+print("{} * {} = {}".format(a, op, b, mul(a, b)))
+print("{} / {} = {}".format(a, op, b, div(a, b)))
