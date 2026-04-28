daf712b323925bd50b852d5a18e9f292e892720e
NelsonFodjo <n.fodjokamd@alustudent.com>
2026-04-28 14:52:04 +0400

    nice

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -5,4 +5,4 @@ def safe_print_division(a, b):
     except ZeroDivisionError:
         return None
     finally:
-        print("Inside result: {}".format(safe_print_division(a, b)))
+        print("Inside result: {}".format(a / b))
