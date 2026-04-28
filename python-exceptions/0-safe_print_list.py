13712425048a7c6d08f566c224b10fd67eb65b70
NelsonFodjo <n.fodjokamd@alustudent.com>
2026-04-28 14:16:16 +0400

    nice

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -5,4 +5,4 @@ def safe_print_list(my_list=[], x=0):
             print(my_list[i], end="")
     except IndexError:
         for i in my_list:
-            print(i, end="")
+            print("{}".format(i), end="")
