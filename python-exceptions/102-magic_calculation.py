05473fe9153a27b944b923b20664fbea61558fe7
NelsonFodjo <n.fodjokamd@alustudent.com>
2026-04-29 16:34:44 +0400

    Enhanced 1 file with improvements
    Changes include: Modified 102-magic_calculation.py
    
    Modified 20 lines (+8 additions, -12 deletions)

--- python-exceptions/102-magic_calculation.py
+++ python-exceptions/102-magic_calculation.py
@@ -1,12 +1,8 @@
-#!/usr/bin/python3
-def magic_calculation(a, b):
-    result = 0
-    for i in range(1, 3):
-        try:
-            if i > a:
-                raise Exception('Too far')
-            result += a ** b / i
-        except Exception:
-            result = b + a
-            break
-    return result
+def addTwo(x):
+    return x + 2
+
+def multiply(a,b):
+    return a * b
+
+var = print(multiply(addTwo(2), 3))
+print(var)
