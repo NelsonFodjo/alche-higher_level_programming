f02425793711b7ebd476a0448ff659ef32779398
NelsonFodjo <n.fodjokamd@alustudent.com>
2026-05-05 00:40:45 +0400

    Enhanced 1 file with improvements
    Changes include: Modified 4-square.py
    
    Added 23 new lines

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+
+
+
+class square:
+    """accessing and updating private attributes"""
+
+    def __init__(self, size=0):
+        self.size = size
+    
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+    
+    @property
+    def size(self):
+        return self.__size
+
+    def area(self):
+        return self.__size ** 2
