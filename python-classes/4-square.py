05d7215c92d5d464905f08464fe33958534fc01d
NelsonFodjo <n.fodjokamd@alustudent.com>
2026-05-05 00:54:25 +0400

    nice

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -6,7 +6,7 @@ class square:
     """accessing and updating private attributes"""
 
     def __init__(self, size=0):
-        self.__size = size
+        self.size = size
     
     @size.setter
     def size(self, value):
