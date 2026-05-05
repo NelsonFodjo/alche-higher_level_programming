d53572a7b5549541823a89fc36546ec81578d67d
NelsonFodjo <n.fodjokamd@alustudent.com>
2026-05-06 00:11:03 +0400

    nice

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -5,7 +5,7 @@
 class Square:
     """A class that defines a square with a private size attribute."""
 
-    def __init__(self, size=0, position=(0,0)):
+    def __init__(self, size=0, position=(0, 0)):
         self.size = size
         self.position = position
 
@@ -29,8 +29,7 @@ class Square:
     def position(self, value):
         if (not isinstance(value, tuple) or
             len(value) != 2 or not isinstance(value[0], int) or
-            not isinstance(value[1], int) or value[0] < 0 or
-            value[1] < 0):
+            not isinstance(value[1], int) or value[0] < 0 or value[1] < 0):
                 raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
