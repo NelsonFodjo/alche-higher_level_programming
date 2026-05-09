339a6a812711f2213bcc38e13cdaac8c81dcdcd7
NelsonFodjo <n.fodjokamd@alustudent.com>
2026-05-09 14:52:44 +0400

    nice

--- python-classes/100-singly_linked_list.py
+++ python-classes/100-singly_linked_list.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
-"""Class Node of a stingly linked list"""
+"""Class Node of a stingly linked list."""
 
 
 class Node:
-    """Class definition of the Node nice"""
+    """Class definition of the Node nice."""
 
     def __init__(self, data, next_node = None):
+        """Innitialize a new Node instance."""
+
         self.data = data
         self.next_node = next_node
     
@@ -25,7 +27,7 @@ class Node:
     
     @next_node.setter
     def next_node(self, value):
-        if value is not None or not isinstance(value, Node):
+        if value is not None and not isinstance(value, Node):
             raise TypeError("next_node must be a Node object")
         self.__next_node = value
 
@@ -34,6 +36,27 @@ class StinglyLinkedList:
 
     def __init__(self):
         self.__head = None
-        
+
+    def __Str__(self):
+        result =""
+        current = self.__head
+        while current is not None:
+            result += str(current.data) + "\n"
+            current = current.next_node
+        return result.strip
+ 
     def sorted_insert(self, value):
+        new_node = Node(value)
+        if self.__head is None or value < self.__head.data:
+            new_node.next_node = self.__head
+            self.head = new_node
+        else:
+            current = self.__head
+            while (current.next_node is not None and
+                   current.next_node.data < value):
+                current = current.next_node
+            new_node.next_node = current.next_node
+            current.next_node = current.next_node
+            current.next_node = new_node
+
 
