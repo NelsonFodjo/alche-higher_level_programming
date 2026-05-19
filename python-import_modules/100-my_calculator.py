8a0f1d7e2a43aff43a5ac82fb6c38fbdd28000fc
NelsonFodjo <n.fodjokamd@alustudent.com>
2026-05-19 23:27:30 +0400

    changed some files

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,11 +1,33 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
+"""This module imports functions from calculator_1 and handles basic operations."""
+import sys
+import calculator_1
+
+
+def main():
+    """Main function to handle calculator operations."""
+    if len(sys.argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+
+    a = int(sys.argv[1])
+    operator = sys.argv[2]
+    b = int(sys.argv[3])
+
+    operators = {
+        '+': calculator_1.add,
+        '-': calculator_1.sub,
+        '*': calculator_1.mul,
+        '/': calculator_1.div,
+    }
+
+    if operator not in operators:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+
+    result = operators[operator](a, b)
+    print("{} {} {} = {}".format(a, operator, b, result))
+
 
 if __name__ == "__main__":
-    a = 10
-    b = 5
-
-print("{} + {} = {}".format(a, b, add(a, b)))
-print("{} - {} = {}".format(a, b, sub(a, b)))
-print("{} * {} = {}".format(a, b, mul(a, b)))
-print("{} / {} = {}".format(a, b, div(a, b)))
+    main()
