6707e05471770ca6ee4e993eb24a9bd953352994
NelsonFodjo <n.fodjokamd@alustudent.com>
2026-05-31 23:00:36 +0400

    fixed

--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -17,6 +17,6 @@ if __name__ == "__main__":
     session = Session()
 
     for state in session.query(State):
-        if "a" in state.name:
+        if "a" in state.name.lower():
             session.delete(state)
     session.commit()
