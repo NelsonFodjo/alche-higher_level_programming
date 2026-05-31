ffd1a417823bfaf94dba7ed8608a4d97737288ec
NelsonFodjo <n.fodjokamd@alustudent.com>
2026-05-31 22:02:44 +0400

    fix: update comments and improve clarity in header and post parameter scripts

--- python-network_0/5-post_params.sh
+++ python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Bash scripts that sends a POST request to a given URL.
-curl -s -X POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD" "$1"
+# Sends a POST request with email and subject variables to the given URL
+curl -s -X POST "$1" -d "email=test@gmail.com" -d "subject=I will always be here for PLD"
