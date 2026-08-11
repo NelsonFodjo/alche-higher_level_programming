b627ff7f8b6c80bb372ad03f5ff3df2eebb70aa0
NelsonFodjo <n.fodjokamd@alustudent.com>
2026-08-11 19:06:20 +0400

    Refactor increment function in 103-object_fct.js for clarity and consistency

--- javascript-warm_up/103-object_fct.js
+++ javascript-warm_up/103-object_fct.js
@@ -4,11 +4,9 @@ const myObject = {
   value: 12
 };
 console.log(myObject);
-
-myObject.incr = function incr () {
-  myObject.value += 1;
+myObject.incr = function () {
+  this.value++;
 };
-
 myObject.incr();
 console.log(myObject);
 myObject.incr();
