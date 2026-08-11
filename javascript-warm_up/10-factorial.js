479cefdb4a54669df11d55da75cc255752f16e24
NelsonFodjo <n.fodjokamd@alustudent.com>
2026-08-11 18:10:23 +0400

    Fix factorial function implementation and add input validation

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -1,2 +1,16 @@
 #!/usr/bin/node
 num = parseInt(process.argv[2], 10);
+
+function factorial (num) {
+  if (num == 1) {
+    return num;
+  } else {
+    return num * factorial(num - 1);
+  }
+}
+
+if (isNaN) {
+  console.log('1');
+} else {
+  console.log(factorial(num));
+}
